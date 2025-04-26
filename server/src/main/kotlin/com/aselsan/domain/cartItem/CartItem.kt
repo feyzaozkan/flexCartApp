c84e3cee8158a931a10f3523c889e80fe20c0004
feyza <feyza@howlet.io>
2025-04-26 11:39:08 +0300

    Implemented Cart Item and Shopping Cart classes

---
+++ server/src/main/kotlin/com/aselsan/domain/cartItem/CartItem.kt
@@ -0,0 +1,9 @@
+package com.aselsan.com.aselsan.domain.cartItem
+
+import com.aselsan.domain.product.Product
+
+class CartItem(
+    product: Product,
+    quantity: Number
+) {
+}
---
+++ server/src/main/kotlin/com/aselsan/domain/shoppingCart/ShoppingCart.kt
@@ -0,0 +1,10 @@
+package com.aselsan.com.aselsan.domain.shoppingCart
+
+import com.aselsan.com.aselsan.domain.cartItem.CartItem
+import com.aselsan.domain.customer.Customer
+
+class ShoppingCart(
+    customer: Customer,
+    items: List<CartItem>
+) {
+}
