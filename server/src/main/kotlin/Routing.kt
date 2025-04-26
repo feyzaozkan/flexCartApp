9bee839200457f258daccce38e02b62256d1e336
feyza <feyza@howlet.io>
2025-04-26 21:17:06 +0300

    Install CORS configuration allowing specific methods, header

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -2,27 +2,38 @@ package com.aselsan
 
 import com.aselsan.com.aselsan.repository.ShoppingRepository
 import com.aselsan.com.aselsan.service.ShoppingService
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-
 import com.aselsan.routes.customerRoutes
 import com.aselsan.routes.productRoutes
 import com.aselsan.routes.shoppingRoutes
 import com.aselsan.service.CustomerService
 import com.aselsan.service.ProductService
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
-            call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
         }
     }
+    install(CORS) {
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+        allowMethod(HttpMethod.Patch)
+        allowHeader(HttpHeaders.Authorization)
+        anyHost()
+    }
+
+
+
     routing {
         swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
         get("/") {
