87ded7e7d4d01f9a5b4b36210cb1ab9ed0b6f085
feyza <feyza@howlet.io>
2025-04-26 13:24:31 +0300

    Percentage Discount, started

---
+++ server/src/main/kotlin/com/aselsan/domain/campaign/PercentageDiscountCampaign.kt
@@ -0,0 +1,28 @@
+package com.aselsan.com.aselsan.domain.campaign
+
+import com.aselsan.com.aselsan.domain.shoppingCart.ShoppingCart
+import com.aselsan.domain.customer.CustomerType
+import com.aselsan.domain.product.ProductCategoryType
+
+class PercentageDiscountCampaign(
+    private val eligibleCategories: Map<ProductCategoryType, Double>, // Category -> Discount Rate
+    private val minCartValue: Double,
+) : Campaign {
+
+    override val name = "Percentage Discount"
+
+    override fun isApplicable(shoppingCart: ShoppingCart): Boolean {
+        return shoppingCart.total >= minCartValue && (shoppingCart.customer.type == CustomerType.PREMIUM)
+    }
+
+    override fun calculateDiscount(shoppingCart: ShoppingCart): Double {
+        if (!isApplicable(shoppingCart)) return 0.0
+
+        var discount = 0.0
+        shoppingCart.items.forEach { item ->
+            val discountRate = eligibleCategories[item.product.category] ?: 0.0
+            discount += item.product.price * discountRate * item.quantity
+        }
+        return discount
+    }
+}
